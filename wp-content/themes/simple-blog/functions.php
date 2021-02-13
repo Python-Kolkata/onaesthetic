ca69d6848ab52fa9a5b3f58d0d2e39dcc51c884e
Rajib Mondal <rajib.3t@gmail.com>
2021-02-13 16:33:19 +0530

    add jquery

--- wp-content/themes/simple-blog/functions.php
+++ wp-content/themes/simple-blog/functions.php
@@ -138,7 +138,7 @@ add_action( 'widgets_init', 'simple_blog_widgets_init' );
 if (!is_admin()) add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
 function my_jquery_enqueue() {
    wp_deregister_script('jquery');
-   wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null,true);
+   wp_register_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js', false, null,false);
    wp_enqueue_script('jquery');
 }
 /**
